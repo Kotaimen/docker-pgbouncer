4db5a8e6cd7f0fc5e1c4be3ce24e731107c5abbb
Kotaimen <kotaimen.c@gmail.com>
2016-02-29 12:07:08 +0800

    Use quiet mode by default

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ chmod -R 755 ${PG_LOG}
 chown -R ${PG_USER}:${PG_USER} ${PG_LOG}
 
 echo "Starting pgbouncer..."
-exec pgbouncer -u ${PG_USER} $PG_CONFIG
+exec pgbouncer -q -u ${PG_USER} $PG_CONFIG
